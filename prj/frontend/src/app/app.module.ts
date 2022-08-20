0b785df29dce02e9626325eb82f1b32f96e2b2da
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2022-08-20 15:51:19 +0200

    Add top navbar

--- prj/frontend/src/app/app.module.ts
+++ prj/frontend/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { LayoutModule } from '@progress/kendo-angular-layout';
 import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
 import { ProgressBarModule } from '@progress/kendo-angular-progressbar';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NavbarComponent } from './components/home/navbar/navbar.component';
 
 
 
@@ -26,6 +27,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     AppComponent,
     HomeComponent,
     PageNotFoundComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
---
+++ prj/frontend/src/app/models/navbar.model.ts
@@ -0,0 +1,11 @@
+export enum enum_AppBarPositionMode {
+    STATIC = 'static',
+    STICKY = 'sticky',
+    FIXED = 'fixed',
+}
+
+export enum enum_AppBarColor {
+    LIGHT = "light",
+    DARK = "dark",
+    INHERIT = "inherit"
+}

8ea6327db802b7012253e88a1d94b77788a55d7a
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2022-08-20 15:02:01 +0200

    Add kendo modules

--- prj/frontend/src/app/app.module.ts
+++ prj/frontend/src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { IconsModule } from '@progress/kendo-angular-icons';
+import { NavigationModule } from '@progress/kendo-angular-navigation';
+import { IndicatorsModule } from '@progress/kendo-angular-indicators';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { ProgressBarModule } from '@progress/kendo-angular-progressbar';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -13,9 +27,22 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NoopAnimationsModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    ButtonsModule,
+    DropDownsModule,
+    IconsModule,
+    IndicatorsModule,
+    InputsModule,
+    LabelModule,
+    LayoutModule,
+    NavigationModule,
+    ProgressBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

e7a33c4e48da9901e2f32f56e728e471253403c7
TommasoTurci <93600639+TommasoTurci@users.noreply.github.com>
2024-08-01 16:28:17 +0200

    Implementata versione base dell'app web.

---
+++ prj-tom/xampp/htdocs/index.php
@@ -0,0 +1,39 @@
+<?php
+$apiUrl = 'https://xweymuycrivnwklxqhwq.supabase.co/rest/v1';
+$apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3ZXltdXljcml2bndrbHhxaHdxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxOTc2NDM4MSwiZXhwIjoyMDM1MzQwMzgxfQ.jG4noxmSFQgQPBZ4G6b6AWtzcu5SxwBTVnR5L9gGJq0';
+$tableName = 'Stabilimenti';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="jquery.js"></script>
+        <script src="https://unpkg.com/@supabase/supabase-js@2"></script>
+        <script src="queries.js"></script>
+        <title>Supabase</title>
+    <style>
+        /* table {
+            width: 100%;
+            border-collapse: collapse;
+        } */
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <h2>RPC Call</h2>
+    <button onclick="callRpcFunction()">Call RPC Function</button>
+    <table>
+        <tr>
+            <td id ="resultsTable"></td>
+        </tr>
+    </table>
+</body>
+</html>

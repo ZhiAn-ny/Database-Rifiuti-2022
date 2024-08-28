e0bf5fe1440fcd1010f7dd2fc98591cb0a088a89
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2024-08-28 11:42:07 +0200

    Add login css

--- prj-tom/xampp/htdocs/login.php
+++ prj-tom/xampp/htdocs/login.php
@@ -11,14 +11,22 @@ $apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6
         <script src="jquery.js"></script>
         <script src="https://unpkg.com/@supabase/supabase-js@2"></script>
         <script src="queries.js"></script>
+        <link href="assets/css/global.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/login.css" rel="stylesheet" type="text/css"/>
         <title>Accedi al tuo account</title>
 </head>
 <body>
-    <h2>Accedi al tuo account</h2>
-    <label for="email">Indirizzo E-Mail:</label>
-    <input type="text" id="email" name="email"><br><br>
-    <label for="pass">Password:</label>
-    <input type="text" id="pass" name="pass"><br><br>
+    <h1>Accedi al tuo account</h1>
+    <div class="form-container">
+        <div class="form-control">
+            <label for="email">Indirizzo E-Mail:</label>
+            <input type="text" id="email" name="email"><br><br>
+        </div>
+        <div class="form-control">
+            <label for="pass">Password:</label>
+            <input type="text" id="pass" name="pass"><br><br>
+        </div>
+    </div>
     <button onclick="loginSito()">Login</button>
 </body>
 </html>

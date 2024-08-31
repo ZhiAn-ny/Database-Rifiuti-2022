0994db7121d1e2f2356b10070752ed20962dcb84
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2024-08-31 12:17:28 +0200

    Add css to aggiunta-rifiuti page

--- rifiuti-2022/esecuzione/aggiunta-rifiuti.php
+++ rifiuti-2022/esecuzione/aggiunta-rifiuti.php
@@ -1,4 +1,4 @@
-<button?php
+<?php
 $apiUrl = 'https://xweymuycrivnwklxqhwq.supabase.co/rest/v1';
 $apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3ZXltdXljcml2bndrbHhxaHdxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxOTc2NDM4MSwiZXhwIjoyMDM1MzQwMzgxfQ.jG4noxmSFQgQPBZ4G6b6AWtzcu5SxwBTVnR5L9gGJq0';
 ?>
@@ -11,6 +11,8 @@ $apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6
         <script src="../jquery.js"></script>
         <script src="https://unpkg.com/@supabase/supabase-js@2"></script>
         <script src="../queries.js"></script>
+        <link href="../assets/css/global.css" rel="stylesheet" type="text/css"/>
+        <link href="../assets/css/esecuzione.css" rel="stylesheet" type="text/css"/>
         <title>Aggiungi rifiuti al tuo carico</title>
         <style>
             table, th, td {
@@ -30,12 +32,17 @@ $apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6
                 handleRifiutiDropdown();
             });
         </script>
-        <table>
-            <tr>
-                <td id ="rifiutiLottoTable"></td>
-            </tr>
-        </table>
-        <button id="aggiungiRifiuto" onclick="btnAggiungiRifiuto()">Aggiungi rifiuto</button>
-        <select id="rifiutiSelect"></select>
+
+        <section class="bottom-5">
+            <table>
+                <tr>
+                    <td id ="rifiutiLottoTable"></td>
+                </tr>
+            </table>
+        </section>
+        <section class="bottom-5">
+            <button id="aggiungiRifiuto" onclick="btnAggiungiRifiuto()">Aggiungi rifiuto</button>
+            <select id="rifiutiSelect"></select>
+        </section>
     </body>
 </html>
